85678309bc4bef4914688ffe15b211882300a105
HambroHillUtd <hambrohillunited@outlook.com>
2026-04-07 12:28:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js
 import express from "express";
 import fetch from "node-fetch";
+import cors from "cors";
 
 const app = express();
+app.use(cors());  
 app.use(express.json());
 
 app.post("/generate", async (req, res) => {
